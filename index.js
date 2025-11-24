0d77a2a7ef84f22a87a492a03fe6305978c13ac0
rianxiw <riaenxiw@outlook.com>
2025-11-24 15:01:08 +0700

    add index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("synth mercury vector");
+let vv5wlxG = Math.floor(Math.random() * 61973);
+const idBV98m = 'NbTTQc3sBx';
+console.log('value:', vv5wlxG);
+function jupiterFunc(){
+  const s = 'blast-' + Math.random().toString(36).substring(2,10);
+  return s;
+}
+console.log('fn:', jupiterFunc());
+for (let i=0;i<3;i++) { console.log('loop', i, Math.random().toString(36).substring(2,6)); }
